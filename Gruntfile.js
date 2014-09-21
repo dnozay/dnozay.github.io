579eb99c97796c0cad7b6a031062150011d57e4e
Damien Nozay <damien.nozay@gmail.com>
2014-09-21 11:50:11 -0700

    don't distrib. assets, those get compiled

--- Gruntfile.js
+++ Gruntfile.js
@@ -256,7 +256,6 @@ module.exports = function (grunt) {
             '!**/_*{,/**}',
             // Explicitly add any files your site needs for distribution here.
             //'_bower_components/jquery/jquery.js',
-            '_bower_components/*/dist/**/*',
             //'favicon.ico',
             //'apple-touch*.png'
           ],
