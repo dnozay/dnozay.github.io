b4b40c3f4548077412ea618f3e002cd8a2f83d4e
Damien Nozay <damien.nozay@gmail.com>
2014-11-04 14:03:51 -0800

    gulp-useref to do some of the usemin stuff

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,13 +1,18 @@
 
-var gulp = require('gulp');
-var serve = require('gulp-serve');
 var spawn = require('child_process').spawn;
 var byline = require('byline');
 var chalk = require('chalk');
 var _ = require('lodash');
-var sass = require('gulp-ruby-sass');
-var rename = require('gulp-rename');
+var gulp = require('gulp');
+
+var cssmin = require('gulp-cssmin');
 var debug = require('gulp-debug');
+var gulpif = require('gulp-if');
+var rename = require('gulp-rename');
+var sass = require('gulp-ruby-sass');
+var serve = require('gulp-serve');
+var uglify = require('gulp-uglify');
+var useref = require('gulp-useref');
 
 var options = {
   yeoman: {
@@ -26,8 +31,24 @@ function _c(template) {
 }
 
 // build html files
-gulp.task('jekyll:serve', function () {
-    var jekyll = spawn('jekyll', ['serve', '--watch',
+gulp.task('html', function () {
+    var paths = [ _c('<%= jekyll.dest %>/**/*.html') ];
+    var searchPath = [
+      _c('<%= yeoman.app %>'),
+      _c('<%= jekyll.dest %>'),
+      _c('<%= yeoman.app %>/_bower_components')
+    ];
+    var assets = useref.assets({searchPath: searchPath});
+    return gulp.src(paths)
+        .pipe(assets)
+        .pipe(gulpif('*.js', uglify()))
+        .pipe(gulpif('*.css', cssmin()))
+        .pipe(assets.restore())
+        .pipe(useref())
+        .pipe(gulp.dest(options.jekyll.dest));
+});
+gulp.task('jekyll:build', function () {
+    var jekyll = spawn('jekyll', ['build',
       '--source',      _c('<%= yeoman.app %>'),
       '--destination', _c('<%= jekyll.dest %>'),
       '--config',      _c('<%= jekyll.config %>'),
@@ -39,7 +60,25 @@ gulp.task('jekyll:serve', function () {
     });
     line_err.on('data', function (data) {
         console.log(chalk.red('[jekyll] ') + data);
-    }); 
+    });
+    jekyll.on('exit', function (code) {
+        console.log('-- Finished Jekyll Build --')
+    })
+});
+gulp.task('jekyll:serve', function () {
+    var jekyll = spawn('jekyll', ['serve', '--skip-initial-build',
+      '--source',      _c('<%= yeoman.app %>'),
+      '--destination', _c('<%= jekyll.dest %>'),
+      '--config',      _c('<%= jekyll.config %>'),
+      '--verbose']);
+    var line_out = byline(jekyll.stdout);
+    var line_err = byline(jekyll.stderr);
+    line_out.on('data', function (data) {
+        console.log(chalk.green('[jekyll] ') + data);
+    });
+    line_err.on('data', function (data) {
+        console.log(chalk.red('[jekyll] ') + data);
+    });
     jekyll.on('exit', function (code) {
         console.log('-- Finished Jekyll Build --')
     })
