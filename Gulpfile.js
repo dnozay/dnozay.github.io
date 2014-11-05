83ed9c77323cdd8686d0a6f54cf3fde03d502fb8
Damien Nozay <damien.nozay@gmail.com>
2014-11-04 22:44:12 -0800

    use gulp-rev-all for asset versioning

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,6 +6,7 @@ var _ = require('lodash');
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
 var lazypipe = require('lazypipe');
+var path = require('path');
 
 var cssmin = require('gulp-cssmin');
 var debug = require('gulp-debug');
@@ -13,6 +14,7 @@ var filerev = require('gulp-rev');
 var gulpif = require('gulp-if');
 var htmlmin = require('gulp-htmlmin');
 var rename = require('gulp-rename');
+var revall = require('gulp-rev-all');
 var sass = require('gulp-ruby-sass');
 var serve = require('gulp-serve');
 var uglify = require('gulp-uglify');
@@ -20,6 +22,7 @@ var useref = require('gulp-useref');
 
 var options = {
   yeoman: {
+    stage: '.tmp',
     app: 'app',
     dist: 'dist'
   },
@@ -47,18 +50,17 @@ gulp.task('styles', function() {
 
 // copy images and fonts
 gulp.task('images', ['stage'], function() {
-  var paths = [ _c('<%= yeoman.app %>/img/**/*.{jpg,jpeg,png}') ];
+  var paths = [ _c('<%= yeoman.app %>/img/**/*.{gif,jpg,jpeg,png,svg,webp}') ];
   var base = _c('<%= yeoman.app %>');
   return gulp.src(paths, {base: base})
-    // .pipe(gulp.dest(options.jekyll.stage))
-    .pipe(gulp.dest(options.yeoman.dist));
+    .pipe(rename({dirname:'img', verbose: false}))
+    .pipe(gulp.dest(options.yeoman.stage));
 });
 gulp.task('fonts', ['stage'], function() {
   var paths = [ _c('<%= yeoman.app %>/**/fonts/**/*.{eot*,otf,svg,ttf,woff}') ];
   return gulp.src(paths)
     .pipe(rename({dirname:'fonts', verbose: false}))
-    // .pipe(gulp.dest(options.jekyll.stage))
-    .pipe(gulp.dest(options.yeoman.dist));
+    .pipe(gulp.dest(options.yeoman.stage));
 });
 
 // build html files
@@ -85,13 +87,9 @@ gulp.task('html:build', ['html:prep'], function (callback) {
 // replace assets references
 gulp.task('html:prep', ['styles'], function () {
     var cssTasks = lazypipe()
-      .pipe(uglify)
-      .pipe(filerev)
-      .pipe(gulp.dest, options.yeoman.dist);
-    var jsTasks =  lazypipe()
-      .pipe(cssmin)
-      .pipe(filerev)
-      .pipe(gulp.dest, options.yeoman.dist);
+      .pipe(cssmin);
+    var jsTasks = lazypipe()
+      .pipe(uglify);
 
     var paths = [
       _c('<%= yeoman.app %>/**/*.{html,yml,md,mkd,markdown}'),
@@ -102,11 +100,17 @@ gulp.task('html:prep', ['styles'], function () {
       _c('<%= jekyll.stage %>'),
       _c('<%= yeoman.app %>/_bower_components')
     ];
-    var assets = useref.assets({searchPath: searchPath});
+    var assets = useref.assets({
+        searchPath: searchPath,
+        types: ['css','js','gif','jpg','jpeg','png','svg','webp']
+    });
     return gulp.src(paths)
         .pipe(assets)
-        .pipe(gulpif('*.js', cssTasks()))
-        .pipe(gulpif('*.css', jsTasks()))
+            .pipe(gulpif('*.js', jsTasks()))
+            .pipe(gulpif('*.css', cssTasks()))
+            // do not use filerev() as it interferes with gulp-rev-all
+            // .pipe(filerev())
+            .pipe(gulp.dest(options.yeoman.stage))
         .pipe(assets.restore())
         .pipe(useref())
         .pipe(gulp.dest(options.jekyll.stage));
@@ -115,20 +119,34 @@ gulp.task('html:prep', ['styles'], function () {
 // since jekyll likes to clobber destination
 // we need to specify a temp directory as a destination.
 gulp.task('stage', ['html:build'], function() {
+    var base        = _c('<%= jekyll.build %>');
     var source      = [ _c('<%= jekyll.build %>/**/*') ];
-    var destination = _c('<%= yeoman.dist %>');
+    var destination = _c('<%= yeoman.stage %>');
     return gulp.src(source)
         .pipe(gulpif('*.html', htmlmin({
-          collapseWhitespace: true,
-          collapseBooleanAttributes: true,
-          removeAttributeQuotes: true,
-          removeRedundantAttributes: true
+          // collapseWhitespace: true,
+          // collapseBooleanAttributes: true,
+          // removeAttributeQuotes: true,
+          // removeRedundantAttributes: true
         })))
         .pipe(gulp.dest(destination));
 });
 
+gulp.task('filerev', ['stage','images','fonts'], function() {
+    var base = _c('<%= yeoman.stage %>');
+    var dist = _c('<%= yeoman.dist %>');
+    var allfiles = _c('<%= yeoman.stage %>/**');
+    return gulp.src(allfiles)
+        .pipe(revall({
+            ignore: [/^\/favicon.ico$/g, '.html'],
+            base: base,
+            hashLength: 8
+        }))
+        .pipe(gulp.dest(dist));
+});
+
 // serve
-gulp.task('serve', ['stage','images','fonts'], function() {
+gulp.task('serve', ['filerev'], function() {
     browserSync({
         server: {
             baseDir: _c('<%= yeoman.dist %>')
